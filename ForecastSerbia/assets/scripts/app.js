35e0cf1695e88750776142083b627c929cd1c1a0
dujakovic <milan.dujakovic@yandex.com>
2022-05-31 10:09:45 +0200

    Created buttons in script file

---
+++ ForecastSerbia/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const buttonSubotica = document.getElementById('btn1');
+const buttonNoviSad = document.getElementById('btn2');
+const buttonBeograd = document.getElementById('btn3');
+const buttonCacak = document.getElementById('btn4');
+const buttonPristina = document.getElementById('btn5');
+const buttonNis = document.getElementById('btn6');
+const buttonKragujevac = document.getElementById('btn7');
